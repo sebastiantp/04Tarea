0c22bbeb1a69da51522ca911586a2a6183b61d3a
Valentino Gonzalez <valentino.g.gonzalez@gmail.com>
2015-10-08 23:25:52 -0300

    Adds bare bones code for guidance

---
+++ planeta.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+class Planeta(object):
+    '''
+    Complete el docstring.
+    '''
+
+    def __init__(self, condicion_inicial, alpha=0):
+        '''
+        __init__ es un método especial que se usa para inicializar las
+        instancias de una clase.
+
+        Ej. de uso:
+        >> mercurio = Planeta([x0, y0, vx0, vy0])
+        >> print(mercurio.alpha)
+        >> 0.
+        '''
+        self.y_actual = condicion_inicial
+        self.t_actual = 0.
+        self.alpha = alpha
+
+    def ecuacion_de_movimiento(self):
+        '''
+        Implementa la ecuación de movimiento, como sistema de ecuaciónes de
+        primer orden.
+        '''
+        x, y, vx, vy = self.y_actual
+        # fx = ...
+        # fy = ...
+        return [vx, vy, fx, fy]
+
+    def avanza_euler(self, dt):
+        '''
+        Toma la condición actual del planeta y avanza su posicion y velocidad
+        en un intervalo de tiempo dt usando el método de Euler explícito. El
+        método no retorna nada, pero re-setea los valores de self.y_actual.
+        '''
+        pass
+
+    def avanza_rk4(self, dt):
+        '''
+        Similar a avanza_euler, pero usando Runge-Kutta 4.
+        '''
+        pass
+
+    def avanza_verlet(self, dt):
+        '''
+        Similar a avanza_euler, pero usando Verlet.
+        '''
+        pass
+
+    def energia_total(self):
+        '''
+        Calcula la enérgía total del sistema en las condiciones actuales.
+        '''
+        pass
+
+
